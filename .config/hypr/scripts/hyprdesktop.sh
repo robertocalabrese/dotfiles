7422bb6bc1e7716577d1d9fce6d2dd2ecb773e8c
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-04-19 07:34:56 +0200

    refactor: remove uninstalled portals

--- .config/hypr/scripts/hyprdesktop.sh
+++ .config/hypr/scripts/hyprdesktop.sh
@@ -4,25 +4,19 @@
 # City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
 # Country:    🅸🆃🅰🅻🆈
 
+# Kill all running portals.
 sleep 1
-
-# kill all possible running xdg-desktop-portals
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal-kde
-#killall xdg-desktop-portal-gnome
-#killall xdg-desktop-portal-kde
-#killall xdg-desktop-portal-lxqt
 #killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-sleep 1
 
-# start xdg-desktop-portal-hyprland
+# Start all the portals again.
+sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
-#/usr/lib/xdg-desktop-portal-gtk &
-#/usr/lib/xdg-desktop-portal-kde &
+/usr/lib/xdg-desktop-portal-gtk &
+/usr/lib/xdg-desktop-portal-kde &
+#/usr/lib/xdg-desktop-portal-wlr &
 sleep 2
-
-# start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
-sleep 1
