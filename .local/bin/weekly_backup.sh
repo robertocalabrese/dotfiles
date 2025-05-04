291bb9f150e59d16c37b8f31870eaa0d055e4565
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-05-04 09:25:59 +0200

    add weekly backup script

---
+++ .local/bin/weekly_backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+######################
+# Incremental Backup #
+######################
+
+echo "Cleaning..."
+sudo pacman -Scc
+
+# Clean the trash bin.
+trash-empty
+
+cd /
+
+# Create the Weekly folder if it doesn't exist yet.
+mkdir -p /mnt/Backups/Weekly
+
+#echo "Checking the Backup mountpoint..."
+#if [  -d "/mnt/Backups" ] ; then
+#    sudo mkdir -p /mnt/Backups
+#    sudo chown -R kicka:users /mnt/Backups
+#fi
+
+#echo "Mounting..."
+#sudo mount UUID=71604311-25f3-44da-979e-c4c8356100b9 /mnt/Backups
+
+# update tldr
+tldr --update
+
+# update pkgfile
+sudo pkgfile --update
+
+# Optional rsync parameter: --dry-run
+echo "Start the Weekly backup..."
+sudo rsync -aAXv --delete   --exclude="/dev/*" \
+                            --exclude="/home/kicka/.cache/*" \
+                            --exclude="/home/kicka/.local/share/*" \
+                            --exclude="/home/kicka/.local/state/*" \
+                            --exclude="/home/kicka/.local/opt/*" \
+                            --exclude="lost+found" \
+                            --exclude="/media/*" \
+                            --exclude="/mnt/*" \
+                            --exclude="/proc/*" \
+                            --exclude="/run/*" \
+                            --exclude="/sys/*" \
+                            --exclude="/tmp/*" \
+                            / /mnt/Backups/Weekly
+
+echo "Syncing..."
+sync
+
+#echo "Unmounting..."
+#sudo umount /mnt/Backups
+
+echo "Backup done."
