52e6f7b6f287df1eb6bdb44e83596f2d6aa987b9
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2025-05-04 09:35:16 +0200

    add waybar script 'sensors'

---
+++ .config/waybar/scripts/sensors.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Author:  Roberto Calabrese a.k.a. Kicka
+# City:    Palermo, Sicily
+# Country: Italy
+
+# ############################
+# ##                        ##
+# ##     SENSORS SCRIPT     ##
+# ##                        ##
+# ############################
+
+sensors && sleep 60
